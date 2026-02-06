6409337f2a6056b3ec5ed22b604e8c7448a6904c
Mrugesh <patilmrugesh84@gmail.com>
2026-02-06 18:26:52 +0530

    favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p className="footer-text">Built with React & CSS</p>
+        <p className="footer-text">Mrugesh Patil</p>
         <p className="footer-copyright">© 2024 All rights reserved</p>
       </div>
     </footer>
